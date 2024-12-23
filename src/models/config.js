5047f2da271fe41eea404cfafbb9bcc4de9cb753
dvmarinoff <dvmarinoff@gmail.com>
2024-12-23 09:03:15 +0200

    add config file

---
+++ src/models/config.js
@@ -0,0 +1,31 @@
+
+class Config {
+    #defaultStravaClientId = 0;
+    #defaultIntervalsClientId = 0;
+    #defaultTrainingPeaksClientId = 0;
+
+    constructor() {
+        this.env = {
+            // PWA_URI: "http://localhost:1234",
+            // API_URI: "http://localhost:8080",
+            PWA_URI: "https://dev.auuki.com",
+            API_URI: "https://api.auuki.com",
+            STRAVA_CLIENT_ID: this.defaultStravaClientId,
+            INTERVALS_CLIENT_ID: this.defaultIntervalsClientId,
+            TRAINING_PEAKS_CLIENT_ID: this.defaultTrainingPeaksClientId,
+        };
+    }
+    setServices(args = {}) {
+        this.env.STRAVA_CLIENT_ID = args.strava ?? this.defaultStravaClientId;
+        this.env.INTERVALS_CLIENT_ID = args.intervals ?? this.defaultIntervalsClientId;
+        this.env.TRAINING_PEAKS_CLIENT_ID = args.trainingPeaks ?? this.defaultTrainingPeaksClientId;
+        Object.freeze(this.env);
+    }
+    get() {
+        return this.env;
+    }
+}
+
+const config = new Config();
+
+export default config;
