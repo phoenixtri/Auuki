e72d75d4f5fe305bcc609b339ed173e3c09ccab9
dvmarinoff <dvmarinoff@gmail.com>
2025-01-04 13:40:20 +0200

    use location.origin for pwa uri

--- src/models/config.js
+++ src/models/config.js
@@ -6,9 +6,9 @@ class Config {
 
     constructor() {
         this.env = {
-            // PWA_URI: "https://localhost:1234",
-            // API_URI: "https://localhost:8080",
-            PWA_URI: "https://auuki.com",
+            // PWA_URI: "http://localhost:1234",
+            // API_URI: "http://localhost:8080",
+            PWA_URI: window.location.origin ?? "https://auuki.com",
             API_URI: "https://api.auuki.com",
             STRAVA_CLIENT_ID: this.defaultStravaClientId,
             INTERVALS_CLIENT_ID: this.defaultIntervalsClientId,
