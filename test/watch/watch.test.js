400089806fcdee5179057ee74866ec340100b7bc
dvmarinoff <dvmarinoff@gmail.com>
2025-02-18 10:52:32 +0200

    add watch test bluerpint
    - need to solve import issue with web workers and jest

--- test/watch/watch.test.js
+++ test/watch/watch.test.js
@@ -2,7 +2,11 @@
  * @jest-environment jsdom
  */
 
+import { JSDOM } from 'jsdom';
+
 import { first, last, xf } from '../../src/functions.js';
+// import { watch } from '../../src/watch.js';
+
 
 describe('Watch', () => {
 
@@ -12,7 +16,23 @@ describe('Watch', () => {
         warn: console.warn,
     };
 
-    test('placeholder', () => {
-        expect(1).toBe(1);
+    jest.useFakeTimers();
+
+    describe.skip('auto start', () => {
+        // first tick
+        xf.dispatch('power', 0);
+        jest.advanceTimersByTime(1000);
+
+        test('0s', () => {
+            expect(watch.elapsed).toBe(0);
+            expect(watch.state).toBe('stopped');
+            expect(watch.stateWorkout).toBe('stopped');
+            expect(watch.autoStartCounter).toBe(3);
+            expect(watch.autoPauseCounter).toBe(0);
+            expect(watch.hasBeenAutoPaused).toBe(false);
+            expect(watch.autoPause).toBe(true);
+            expect(watch.autoStart).toBe(true);
+        });
     });
 });
+
